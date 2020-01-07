1c1b3038abcafbd1253ccdcefdc8d3856dadda36
Nayan Chakrabarty <nayandcc5@gmail.com>
2020-01-07 15:59:51 +0600

    card update

--- app/src/main/java/com/nexttech/easybusinesscard/Create_card.java
+++ app/src/main/java/com/nexttech/easybusinesscard/Create_card.java
@@ -1,16 +1,35 @@
 package com.nexttech.easybusinesscard;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class Create_card extends AppCompatActivity {
-    TextView importtemp,share,export,help;
+    TextView importtemp,share,export,browse;
+
+
     View v1,v2,v3,v5;
-    View focusview;
+    View focusview, dialogueView;
+    AlertDialog.Builder builder;
+    AlertDialog alertDialog;
+    ImageView mainTemp;
     boolean b=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +38,13 @@ public class Create_card extends AppCompatActivity {
         importtemp=findViewById(R.id.importtemp);
         share=findViewById(R.id.share);
         export=findViewById(R.id.export);
-        help=findViewById(R.id.help);
+        browse=findViewById(R.id.browse);
         v1=findViewById(R.id.view1);
         v2=findViewById(R.id.view2);
         v3=findViewById(R.id.view3);
         v5=findViewById(R.id.view5);
-
+        mainTemp = findViewById(R.id.maintemp);
+        builder = new AlertDialog.Builder(Create_card.this);
         importtemp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +73,7 @@ public class Create_card extends AppCompatActivity {
                     focusview=v2;
                     b=true;
                 }
+                showShareDialougeBox();
             }
         });
         export.setOnClickListener(new View.OnClickListener() {
@@ -70,7 +91,7 @@ public class Create_card extends AppCompatActivity {
                 }
             }
         });
-        help.setOnClickListener(new View.OnClickListener() {
+        browse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(b){
@@ -83,8 +104,99 @@ public class Create_card extends AppCompatActivity {
                     focusview=v5;
                     b=true;
                 }
+                ShowDialogebox();
+
             }
         });
 
+    }
+    void ShowDialogebox()
+    {
+        ImageView temp1, temp1rear, temp2,temp2rear, temp3, temp3rear;
+
+        dialogueView = getLayoutInflater().inflate(R.layout.brows_template, null);
+        temp1 = dialogueView.findViewById(R.id.imgtemp1);
+        temp1rear = dialogueView.findViewById(R.id.imgtemp1rear);
+        temp2 = dialogueView.findViewById(R.id.imgtemp2);
+        temp2rear = dialogueView.findViewById(R.id.imgtemp2rear);
+        temp3 = dialogueView.findViewById(R.id.imgtemp3);
+        temp3rear = dialogueView.findViewById(R.id.imgtemp3rear);
+
+        builder.setView(null);
+        builder.setView(dialogueView);
+        alertDialog=builder.create();
+        alertDialog.setCanceledOnTouchOutside(true);
+        alertDialogDismiss();
+        alertDialog.show();
+
+        temp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainTemp.setImageDrawable(getResources().getDrawable(R.drawable.cardone));
+                alertDialogDismiss();
+            }
+        });
+        temp1rear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainTemp.setImageDrawable(getResources().getDrawable(R.drawable.rear));
+                alertDialogDismiss();
+            }
+        });
+        temp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainTemp.setImageDrawable(getResources().getDrawable(R.drawable.temp2));
+                alertDialogDismiss();
+            }
+        });
+        temp2rear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainTemp.setImageDrawable(getResources().getDrawable(R.drawable.temp2rear));
+                alertDialogDismiss();
+
+            }
+        });
+        temp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainTemp.setImageDrawable(getResources().getDrawable(R.drawable.temp3));
+                alertDialogDismiss();
+            }
+        });
+        temp3rear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainTemp.setImageDrawable(getResources().getDrawable(R.drawable.temp3rear));
+                alertDialogDismiss();
+            }
+        });
+
+    }
+    private void alertDialogDismiss(){
+        if (alertDialog.isShowing()){
+            alertDialog.dismiss();
+        }
+    }
+    void showShareDialougeBox(){
+
+        BitmapDrawable drawable = (BitmapDrawable) mainTemp.getDrawable();
+        Bitmap bitmap1 = drawable.getBitmap();
+
+
+
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/*");
+        share.putExtra(Intent.EXTRA_STREAM, getImageUri(this,bitmap1));
+        startActivity(Intent.createChooser(share,"Share via"));
+    }
+
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 }
