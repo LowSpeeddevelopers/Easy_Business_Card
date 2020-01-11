c641d1d7ece8e1fdc2f4a07a676997e9fe7a757f
Masum Nishat <masum.nishat21@gmail.com>
2020-01-11 18:48:32 +0600

    added drag listener

--- app/src/main/java/com/nexttech/easybusinesscard/Create_card.java
+++ app/src/main/java/com/nexttech/easybusinesscard/Create_card.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
@@ -17,7 +21,10 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsoluteLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,7 +35,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Random;
 
-public class Create_card extends AppCompatActivity {
+public class Create_card extends AppCompatActivity implements View.OnDragListener, View.OnLongClickListener{
     TextView importtemp,share,export,browse;
 
 
@@ -37,7 +44,11 @@ public class Create_card extends AppCompatActivity {
     AlertDialog.Builder builder;
     AlertDialog alertDialog;
     ImageView mainTemp, ivLockCard;
+    TextView textfield;
+    AbsoluteLayout absoluteLayout;
     boolean b=false;
+    int x;
+    int y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +62,23 @@ public class Create_card extends AppCompatActivity {
         v2=findViewById(R.id.view2);
         v3=findViewById(R.id.view3);
         v5=findViewById(R.id.view5);
+        absoluteLayout=findViewById(R.id.absolutelayout);
         mainTemp = findViewById(R.id.maintemp);
+        textfield=findViewById(R.id.textFieldTextview);
+
+
+        absoluteLayout.setOnDragListener(this);
+        textfield.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView=new TextView(Create_card.this);
+                textView.setTag("draggable textview");
+                textView.setText("type your text here");
+                textView.setOnLongClickListener(Create_card.this);
+
+                absoluteLayout.addView(textView);
+            }
+        });
         builder = new AlertDialog.Builder(Create_card.this);
         importtemp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -131,11 +158,8 @@ public class Create_card extends AppCompatActivity {
     }
 
     private void SaveImage(Bitmap finalBitmap) {
-
         String root = Environment.getExternalStorageDirectory().toString();
-
         String saveDirectoryName = "Business Cards";
-
         File myDir = new File(root + "/"+saveDirectoryName);
         myDir.mkdirs();
         Random generator = new Random();
@@ -154,7 +178,6 @@ public class Create_card extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
     public boolean isStoragePermissionGranted() {
         if (Build.VERSION.SDK_INT >= 23) {
             if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -162,7 +185,6 @@ public class Create_card extends AppCompatActivity {
                 Log.e("Permission","Permission is granted");
                 return true;
             } else {
-
                 Log.v("Permission","Permission is revoked");
                 ActivityCompat.requestPermissions(Create_card.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
                 return false;
@@ -262,5 +284,136 @@ public class Create_card extends AppCompatActivity {
         inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
         String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
         return Uri.parse(path);
+    }
+
+
+
+    @Override
+    public boolean onLongClick(View v) {
+        // Create a new ClipData.Item from the ImageView object's tag
+        ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+        // Create a new ClipData using the tag as a label, the plain text MIME type, and
+        // the already-created item. This will create a new ClipDescription object within the
+        // ClipData, and set its MIME type entry to "text/plain"
+        String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+        ClipData data = new ClipData(v.getTag().toString(), mimeTypes, item);
+        // Instantiates the drag shadow builder.
+        View.DragShadowBuilder dragshadow = new View.DragShadowBuilder(v);
+        // Starts the drag
+        v.startDrag(data        // data to be dragged
+                , dragshadow   // drag shadow builder
+                , v           // local data about the drag and drop operation
+                , 0          // flags (not currently used, set to 0)
+        );
+        return true;
+    }
+    // This is the method that the system calls when it dispatches a drag event to the listener.
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        // Defines a variable to store the action type for the incoming event
+        int action = event.getAction();
+        // Handles each of the expected events
+        switch (action) {
+
+            case DragEvent.ACTION_DRAG_STARTED:
+                // Determines if this View can accept the dragged data
+                if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+                    // if you want to apply color when drag started to your view you can uncomment below lines
+                    // to give any color tint to the View to indicate that it can accept data.
+                    //v.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
+                    //Invalidate the view to force a redraw in the new tint
+                    //  v.invalidate();
+                    // returns true to indicate that the View can accept the dragged data.
+                    return true;
+                }
+                // Returns false. During the current drag and drop operation, this View will
+                // not receive events again until ACTION_DRAG_ENDED is sent.
+                return false;
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+                // Applies a GRAY or any color tint to the View. Return true; the return value is ignored.
+                v.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN);
+                // Invalidate the view to force a redraw in the new tint
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DRAG_LOCATION:
+                // Ignore the event
+
+                Point touchPosition = getTouchPositionFromDragEvent(v, event);
+
+                x = touchPosition.x;
+                y = touchPosition.y;
+
+
+                Log.e("x",Integer.toString(x));
+                Log.e("y",Integer.toString(y));
+
+
+                return true;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+                // Re-sets the color tint to blue. Returns true; the return value is ignored.
+                // view.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
+                //It will clear a color filter .
+                v.getBackground().clearColorFilter();
+                // Invalidate the view to force a redraw in the new tint
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DROP:
+                // Gets the item containing the dragged data
+                ClipData.Item item = event.getClipData().getItemAt(0);
+                // Gets the text data from the item.
+                String dragData = item.getText().toString();
+                // Displays a message containing the dragged data.
+                Toast.makeText(this, "Dragged data is " + dragData, Toast.LENGTH_SHORT).show();
+                // Turns off any color tints
+                v.getBackground().clearColorFilter();
+                // Invalidates the view to force a redraw
+                v.invalidate();
+
+                View vw = (View) event.getLocalState();
+                ViewGroup owner = (ViewGroup) vw.getParent();
+                owner.removeView(vw); //remove the dragged view
+                //caste the view into LinearLayout as our drag acceptable layout is LinearLayout
+                vw.setX(x-vw.getWidth()/2);
+                vw.setY(y-vw.getHeight()/2);
+
+
+                Log.e("set x",Float.toString(vw.getX()));
+                Log.e("set y",Float.toString(vw.getY()));
+
+
+                AbsoluteLayout container = (AbsoluteLayout) v;
+                container.addView(vw);//Add the dragged view
+                vw.setVisibility(View.VISIBLE);//finally set Visibility to VISIBLE
+                // Returns true. DragEvent.getResult() will return true.
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+                // Turns off any color tinting
+                v.getBackground().clearColorFilter();
+                // Invalidates the view to force a redraw
+                v.invalidate();
+                // Does a getResult(), and displays what happened.
+                if (event.getResult())
+                    Toast.makeText(this, "The drop was handled.", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, "The drop didn't work.", Toast.LENGTH_SHORT).show();
+                // returns true; the value is ignored.
+                return true;
+            // An unknown action type was received.
+            default:
+                Log.e("DragDrop Example", "Unknown action type received by OnDragListener.");
+                break;
+        }
+        return false;
+    }
+
+    public Point getTouchPositionFromDragEvent(View item, DragEvent event) {
+        // Rect rItem = new Rect();
+        // item.getGlobalVisibleRect(rItem);
+        return new Point( Math.round(event.getX()),  Math.round(event.getY()));
     }
 }
