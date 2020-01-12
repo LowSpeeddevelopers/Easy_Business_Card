bf4f6f92d4a365f5719e75bfd0233994ab2672dd
Nayan Chakrabarty <nayandcc5@gmail.com>
2020-01-12 12:34:32 +0600

    card update

---
+++ app/src/main/java/com/nexttech/easybusinesscard/TextFragment.java
@@ -0,0 +1,15 @@
+package com.nexttech.easybusinesscard;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TextFragment extends Fragment {
+
+}
