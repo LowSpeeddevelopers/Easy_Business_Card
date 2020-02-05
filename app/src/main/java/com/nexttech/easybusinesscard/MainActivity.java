96868a8325eeeaad1401c3e954f93494b5b55ec7
Nayan Chakrabarty <nayandcc5@gmail.com>
2020-02-05 15:06:52 +0600

    employee signup update

--- app/src/main/java/com/nexttech/easybusinesscard/MainActivity.java
+++ app/src/main/java/com/nexttech/easybusinesscard/MainActivity.java
@@ -8,27 +8,38 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
 
     ViewPager viewPager;
 
+    FirebaseAuth firebaseAuth;
+    String userId;
+    FirebaseUser firebaseUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         setSupportActionBar(null);
         Toolbar toolbar = findViewById(R.id.toolbarMainActivity);
-
         TextView toolbartext = toolbar.findViewById(R.id.toolbartext);
         toolbartext.setText("LOG IN");
+        firebaseAuth=FirebaseAuth.getInstance();
+        //userId=firebaseAuth.getCurrentUser().getUid();
+        firebaseUser=firebaseAuth.getCurrentUser();
+
         viewPager = findViewById(R.id.container);
         viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
     }
 
 
@@ -41,13 +52,31 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public Fragment getItem(int pos) {
 
-            return new signUp_Type(MainActivity.this);
+            switch (pos){
+                case 0:
+                   return new Login_fragment(MainActivity.this);
+                case 1:
+                    return new signUp_Type(MainActivity.this);
+                default:
+                    return new EmployeeSignUp(MainActivity.this);
+            }
         }
 
         @Override
         public int getCount() {
-            return 1;
+            return 2;
         }
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(firebaseUser!=null){
+            startActivity(new Intent(this,Create_card.class));
+        }else {
+            viewPager.setCurrentItem(0);
+        }
+    }
 }
