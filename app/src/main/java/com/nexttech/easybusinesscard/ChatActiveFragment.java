d3c53f794c02416f541193f1197aefac23ed8946
GKSnigdha <57640315+GKSnigdha@users.noreply.github.com>
2020-02-20 16:05:47 +0600

    chat Active

---
+++ app/src/main/java/com/nexttech/easybusinesscard/ChatActiveFragment.java
@@ -0,0 +1,27 @@
+package com.nexttech.easybusinesscard;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class ChatActiveFragment extends Fragment {
+
+    public ChatActiveFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_active, container, false);
+    }
+}
--- app/src/main/java/com/nexttech/easybusinesscard/Job/Fragment/ChatListFragment.java
+++ app/src/main/java/com/nexttech/easybusinesscard/Job/Fragment/ChatListFragment.java
@@ -11,21 +11,18 @@ import android.view.ViewGroup;
 
 import com.nexttech.easybusinesscard.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ChatListFragment extends Fragment {
 
 
     public ChatListFragment() {
-        // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        
         return inflater.inflate(R.layout.fragment_chat_list, container, false);
     }
 
